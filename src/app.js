3df3461f4850e4b76ddb5c405b6f449a423c6ad0
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-07-04 12:15:52 -0300

    Alterando js post

--- src/app.js
+++ src/app.js
@@ -19,8 +19,8 @@ app.get('', (req, res) => {
     res.render('index')
 })
 
-const port = process.env.port || 3000
+//const port = process.env.port || 3000
 
-app.listen(port, () => {
-    console.log(`Server is up at the port ${port}`)
+app.listen(3001, () => {
+    console.log(`Server is up at the port `)
 })
