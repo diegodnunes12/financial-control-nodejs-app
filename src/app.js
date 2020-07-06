c13588a377a640a151a59ea6523e37e620377d5e
Diego Dleon Nunes <diegodnunes12@gmail.com>
2020-07-06 19:37:13 -0300

    Lendo o valor de get orders

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const request = require('request')
 
 const app = express()
 
@@ -14,6 +15,53 @@ app.set('views', pathViews)
 hbs.registerPartials(pathPartials)
 
 app.use(express.static(pathPublic))
+
+const ords = (callback) => {   
+
+    const url = `http://localhost:3000/orders`
+
+    request({url: url, json: true}, (err, response) => {
+        if(err){
+            callback({
+                message : `Something went wrong ${err}`,
+                code : 500
+            }, undefined)
+           
+        }
+
+        console.log(response.body[0])
+        
+        if(response.body === undefined){
+            callback({
+                message : `No data found`,
+                code : 404
+            }, undefined)
+        }else{
+            const parseJson = response.body[0]
+
+            const {name} = parseJson
+
+            callback(undefined, {name})     
+        }   
+    })
+}
+
+app.get('/orders', (req, res) => {
+
+    ords((err, body) => {
+        if(err){
+
+            return res.status(err.code).json({error : {
+                message : 'Not Found',
+                code : err.code
+            }})
+        }
+
+        console.log(body)
+        res.status(200).json(body)
+    }) 
+})
+
 
 app.get('', (req, res) => {
     res.render('index')
